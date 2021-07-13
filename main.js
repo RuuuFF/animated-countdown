f451f3583b4d2bc39659f256386dd3525f086fc3
Carlos Daniel <dev.cdfaria@gmail.com>
2021-07-13 18:18:15 -0300

    refactoring the code and adding elements by js

--- main.js
+++ main.js
@@ -1,38 +1,60 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
+const Countdown = {
+  container: document.querySelector('.nums'),
+  counter: document.querySelector('.counter'),
+  finalMessage: document.querySelector('.final'),
+  replay: document.querySelector('#replay'),
+  
+  nums: null,
+  count: 3,
 
-runAnimation()
+  reset(nums) {
+    Countdown.counter.classList.remove('hide')
+    Countdown.finalMessage.classList.remove('show')
 
-function resetDOM() {
-  counter.classList.remove('hide')
-  finalMessage.classList.remove('show')
+    nums.forEach(num => num.classList.value = '')
 
-  nums.forEach(num => num.classList.value = '')
+    nums[0].classList.add('in')
+  },
 
-  nums[0].classList.add('in')
-}
-
-function runAnimation() {
-  nums.forEach((num, index) => {
-    const nextToLast = nums.length - 1
-
-    num.addEventListener('animationend', event => {
-      if (event.animationName === 'goIn' && index !== nextToLast) {
-        num.classList.remove('in')
-        num.classList.add('out')
-      } else if (event.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
-        counter.classList.add('hide')
-        finalMessage.classList.add('show')
-      }
+  runAnimation(nums) {
+    nums.forEach((num, index) => {
+      const nextToLast = nums.length - 1
+  
+      num.addEventListener('animationend', event => {
+        if (event.animationName === 'goIn' && index !== nextToLast) {
+          num.classList.remove('in')
+          num.classList.add('out')
+        } else if (event.animationName === 'goOut' && num.nextElementSibling) {
+          num.nextElementSibling.classList.add('in')
+        } else {
+          Countdown.counter.classList.add('hide')
+          Countdown.finalMessage.classList.add('show')
+        }
+      })
     })
-  })
+  },
+
+  createCounterEl() {
+    Countdown.container.innerHTML = ''
+
+    for (Countdown.count; Countdown.count >= 0; Countdown.count--) {
+      const numEl = document.createElement('span')
+
+      numEl.innerText = Countdown.count
+
+      Countdown.container.appendChild(numEl)
+    }
+
+    Countdown.nums = document.querySelectorAll('.nums span')
+
+    Countdown.nums[0].classList.add('in')
+    Countdown.runAnimation(Countdown.nums)
+  },
+
+  start() {
+    Countdown.createCounterEl()
+    Countdown.replay.addEventListener('click', () => Countdown.reset(Countdown.nums))
+  }
 }
 
-replay.addEventListener('click', () => {
-  resetDOM()
-  runAnimation()
-})
+Countdown.start()
